b8979de73904a501a6d6c5ed4a960b353735dd6a
Cheshire Cat <thacheshirecat@gmail.com>
2018-08-03 11:45:07 -0700

    Add a method to convert seconds back into years with an accuracy of 2 decimal places.

--- spec/galactic-age-spec.js
+++ spec/galactic-age-spec.js
@@ -4,14 +4,20 @@ describe('GalacticAge', function()
 {
   it('should test if the GetAgeInSeconds method returns the correct estimated number of seconds someone has been alive on earth based on their age alone', function()
   {
-    let testAge = new GalacticAge(30)
+    let testAge = new GalacticAge(30);
     expect(946080000).toEqual(testAge.GetAgeInSeconds());
   });
   it('should test if the CompareDatesInSeconds method correctly returns the correct estimated number of seconds between two given dates', function()
   {
-    let testLaterDate = new GalacticAge(0, 2018, 7, 3)
-    let testEarlierDate = new GalacticAge(0, 2015, 11, 25)
-    expect(82339200).toEqual(testLaterDate.CompareDatesInSeconds(testEarlierDate));
+    let testLaterDate = new GalacticAge(0, 2018, 7, 3);
+    let testEarlierDate = new GalacticAge(0, 2015, 11, 25);
+    expect(82339200).toEqual(testEarlierDate.CompareDatesInSeconds(testLaterDate));
+  });
+  it('should test if the ConvertBackToYears method correctly returns the number of years baseed on how many seconds are in a GalacticAge', function()
+  {
+    let testDate = new GalacticAge(0, 0, 0, 0, 82339200);
+    console.log(testDate.ConvertBackToYears());
+    expect(2.61).toEqual(testDate.ConvertBackToYears());
   });
 
 });
--- src/galactic-age.js
+++ src/galactic-age.js
@@ -1,25 +1,32 @@
 export class GalacticAge
 {
-  constructor(earthAge, earthYear, earthMonth, earthDay)
+  constructor(earthAge, earthYear, earthMonth, earthDay, earthSeconds)
   {
     this.earthAge = earthAge;
     this.earthYear = earthYear;
     this.earthMonth = earthMonth;
     this.earthDay = earthDay;
+    this.earthSeconds = earthSeconds;
   }
   GetAgeInSeconds()
   {
     let ageInSeconds = ((((this.earthAge * 365) * 24) * 60) * 60);
+    this.earthSeconds = ageInSeconds;
     return ageInSeconds;
   }
-  CompareDatesInSeconds(earlierDate)
+  CompareDatesInSeconds(laterDate)
   {
-    let yearDifference = (((((this.earthYear - earlierDate.earthYear) * 365) * 24) * 60) * 60);
-    let monthDifference = (((((this.earthMonth - earlierDate.earthMonth) * 30) * 24) * 60) * 60);
-    let dayDifference = ((((this.earthDay - earlierDate.earthDay) * 24) * 60) * 60);
+    let yearDifference = (((((laterDate.earthYear - this.earthYear) * 365) * 24) * 60) * 60);
+    let monthDifference = (((((laterDate.earthMonth - this.earthMonth) * 30) * 24) * 60) * 60);
+    let dayDifference = ((((laterDate.earthDay - this.earthDay) * 24) * 60) * 60);
 
     let differenceInSeconds = yearDifference + monthDifference + dayDifference;
-
+    this.earthSeconds = differenceInSeconds;
     return differenceInSeconds;
+  }
+  ConvertBackToYears()
+  {
+    let ageInYears = ((((this.earthSeconds / 60) / 60) / 24) / 365);
+    return parseFloat(ageInYears.toFixed(2));
   }
 }
