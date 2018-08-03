fbfde5034a9777022505a80598acb863772f0606
Cheshire Cat <thacheshirecat@gmail.com>
2018-08-03 11:23:30 -0700

    Add a CompareDatesInSeconds method along with a passing test. This method is passed on one GalacticAge and takes in another Galactic age in order to function.

--- spec/galactic-age-spec.js
+++ spec/galactic-age-spec.js
@@ -2,9 +2,16 @@ import { GalacticAge } from './../src/galactic-age';
 
 describe('GalacticAge', function()
 {
-  it('should test if the GetAgeInSeconds method correctly returns the correct estimated number of seconds someone has been alive on earth', function()
+  it('should test if the GetAgeInSeconds method returns the correct estimated number of seconds someone has been alive on earth based on their age alone', function()
   {
     let testAge = new GalacticAge(30)
     expect(946080000).toEqual(testAge.GetAgeInSeconds());
   });
+  it('should test if the CompareDatesInSeconds method correctly returns the correct estimated number of seconds between two given dates', function()
+  {
+    let testLaterDate = new GalacticAge(0, 2018, 7, 3)
+    let testEarlierDate = new GalacticAge(0, 2015, 11, 25)
+    expect(82339200).toEqual(testLaterDate.CompareDatesInSeconds(testEarlierDate));
+  });
+
 });
--- src/galactic-age.js
+++ src/galactic-age.js
@@ -12,4 +12,14 @@ export class GalacticAge
     let ageInSeconds = ((((this.earthAge * 365) * 24) * 60) * 60);
     return ageInSeconds;
   }
+  CompareDatesInSeconds(earlierDate)
+  {
+    let yearDifference = (((((this.earthYear - earlierDate.earthYear) * 365) * 24) * 60) * 60);
+    let monthDifference = (((((this.earthMonth - earlierDate.earthMonth) * 30) * 24) * 60) * 60);
+    let dayDifference = ((((this.earthDay - earlierDate.earthDay) * 24) * 60) * 60);
+
+    let differenceInSeconds = yearDifference + monthDifference + dayDifference;
+
+    return differenceInSeconds;
+  }
 }
